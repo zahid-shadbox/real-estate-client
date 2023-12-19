7baa503c0c2b473be3f8e6886bb8c822c16b68a6
zahid-shadbox <zahidshadbox@gmail.com>
2023-12-19 19:08:47 +0530

    added search bar and stats

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import "./Hero.css";
-import {HiLocationMarker} from "react-icons/hi";
+import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
 
 const Hero = () => {
   return (
@@ -8,7 +9,7 @@ const Hero = () => {
         {/*Left Side */}
         <div className="flexColStart hero-left">
           <div className="hero-title">
-          <div className="orange-circle"/>
+            <div className="orange-circle" />
             <h1>
               Discover <br />
               Most Suitable <br />
@@ -19,8 +20,33 @@ const Hero = () => {
             <span>Find a variety of properties that suit you very easilty</span>
             <span>Forget all difficulties in finding a residence for you</span>
           </div>
-          <div className="search-bar">
-          <HiLocationMarker color="var(--blue)" size={25}/>          
+          <div className="flexCenter search-bar">
+            <HiLocationMarker color="var(--blue)" size={25} />
+            <input type="text" />
+            <button className="button">Search</button>
+          </div>
+
+          <div className="flexCenter stats">
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={8800} end={9000} duration={4} /> <span>+</span>
+              </span>
+              <span className="secondaryText">Premium Product</span>
+            </div>
+
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={1950} end={2000} duration={4} /> <span>+</span>
+              </span>
+              <span className="secondaryText">Happy Customer</span>
+            </div>
+
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp end={28} /> <span>+</span>
+              </span>
+              <span className="secondaryText">Awards Winning</span>
+            </div>
           </div>
         </div>
         {/*Right Side */}
