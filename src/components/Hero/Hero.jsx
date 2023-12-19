595e72e48c36378f56b34c76a87f356552b9e46d
zahid-shadbox <zahidshadbox@gmail.com>
2023-12-19 17:44:42 +0530

    added styles to hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,14 @@
 import "./Hero.css";
+import {HiLocationMarker} from "react-icons/hi";
 
 const Hero = () => {
   return (
     <section className="hero-wrapper">
       <div className="paddings innerWidth flexCenter hero-container">
         {/*Left Side */}
-        <div className="hero-left">
+        <div className="flexColStart hero-left">
           <div className="hero-title">
+          <div className="orange-circle"/>
             <h1>
               Discover <br />
               Most Suitable <br />
@@ -16,6 +18,9 @@ const Hero = () => {
           <div className="flexColStart hero-des">
             <span>Find a variety of properties that suit you very easilty</span>
             <span>Forget all difficulties in finding a residence for you</span>
+          </div>
+          <div className="search-bar">
+          <HiLocationMarker color="var(--blue)" size={25}/>          
           </div>
         </div>
         {/*Right Side */}
