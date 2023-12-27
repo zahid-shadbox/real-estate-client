89aefb78f892d849849552d4243d7be23046e3bc
zahid-shadbox <zahidshadbox@gmail.com>
2023-12-27 21:06:44 +0530

    added hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Website from "./pages/Website";
 import "./App.css";
 import { Suspense } from "react";
 import Layout from "./components/Layout/Layout";
 import Properties from "./pages/Properties/Properties";
+import { QueryClient, QueryClientProvider } from "react-query";
+import {  ReactQueryDevtools } from "react-query/devtools";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <Router>
-    <Suspense fallback={<div>Loading...</div>}>
-    <Routes>
-    <Route element={<Layout/>}>
-    <Route path="/" element={<Website/>}/>
-    <Route path="/properties" element={<Properties/>}/>
-    </Route>
-    </Routes>
-    </Suspense>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Website />} />
+              <Route path="/properties" element={<Properties />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </Router>
+      <ToastContainer/>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
--- src/pages/Properties/Properties.jsx
+++ src/pages/Properties/Properties.jsx
@@ -1,13 +1,45 @@
+import useProperties from "../../Hooks/useProperties"
+import PropertyCard from "../../components/PropertyCard/PropertyCard"
 import SearchBar from "../../components/SearchBar/SearchBar"
 import "./Properties.css"
+import {PuffLoader} from "react-spinners"
 
 const Properties = () => {
 
+const {data,isError, isLoading} = useProperties()
+console.log(data)
 
+if (isError){
+  return(
+    <div className="wrapper">
+      <span>Error while fetching data</span>
+    </div>
+  )
+}
+if(isLoading){
+  return(
+    <div className="wrapper" style={{height:"60vh"}}>
+      <PuffLoader 
+      height="80"
+      width="80"
+      radius={1}
+      color="#4066ff"
+      arai-label="puff-loading"
+      />
+    </div>
+  )
+}
   return (
     <div className='wrapper'>
       <div className="flexColCenter paddings innerWidth properties-container">
         <SearchBar/>
+        <div className="paddings flexCenter properties">
+        {
+          data.map((card, i)=>(
+            <PropertyCard card={card} key={i}/>
+          ))
+        }
+        </div>
       </div>
     </div>
   )
