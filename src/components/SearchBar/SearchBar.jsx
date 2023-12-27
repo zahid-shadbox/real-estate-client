a1163bceb9da8d571ab679c367ab8d42cc34dde5
zahid-shadbox <zahidshadbox@gmail.com>
2023-12-27 19:00:11 +0530

    modified properties page and search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,7 +3,7 @@ import { HiLocationMarker } from 'react-icons/hi'
 
 const SearchBar = () => {
   return (
-    <div className="flexCenter search-bar">
+    <div className="flexCenter search-bar" style={{justifyContent:"space-between"}}>
     <HiLocationMarker color="var(--blue)" size={25} />
     <input type="text" />
     <button className="button">Search</button>
--- src/pages/Properties/Properties.jsx
+++ src/pages/Properties/Properties.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
 import SearchBar from "../../components/SearchBar/SearchBar"
+import "./Properties.css"
 
 const Properties = () => {
+
+
   return (
     <div className='wrapper'>
-    <div className="flexColCenter paddings innerWidth properties-container">
-    <SearchBar/>
-    </div>
+      <div className="flexColCenter paddings innerWidth properties-container">
+        <SearchBar/>
+      </div>
     </div>
   )
 }
